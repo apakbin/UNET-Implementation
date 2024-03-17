60a6a654d8ef1deb9f386e65e3446d7b3e52a642
Pakbin <a.pakbin@tamu.edu>
2024-03-17 15:27:29 -0500

    added in_channels/out_channels to test

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from model import DownsampleBlock
 if __name__ == "__main__":
 
 
-    channels    = 4
-    fbins       = 256
-    T           = 100
-    pool_k_size = (2, 1)
-    conv_k_size = (3, 3)
-    conv_stride = (1, 1)
-    db = DownsampleBlock(channels, pool_k_size, conv_k_size, conv_stride)
+    in_channels  = 4
+    out_channels = 8
+    fbins        = 256
+    T            = 100
+    pool_k_size  = (2, 1)
+    conv_k_size  = (3, 3)
+    conv_stride  = (1, 1)
+    db = DownsampleBlock(in_channels, out_channels, pool_k_size, conv_k_size, conv_stride)
 
-    input = torch.rand(channels, fbins, T)
+    input = torch.rand(in_channels, fbins, T)
     output = db(input)
-    assert (tuple(output.shape) == (channels, fbins//2, T)), "ERROR: Shape mismatch in DownsampleBlock!"
+    assert (tuple(output.shape) == (out_channels, fbins//2, T)), "ERROR: Shape mismatch in DownsampleBlock!"
